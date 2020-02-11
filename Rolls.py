2b9631e3433566e898c23b95fe14b3ca8ce3825d
salnigro <salnigro627@gmail.com>
2020-02-10 18:56:36 -0600

    dice

--- Rolls.py
+++ Rolls.py
@@ -4,14 +4,13 @@ n = int(input("Enter number of Dice: "))
 x = 0
 def Dicerolls(n):
 	for i in range(0,n):
-		x = random.randrange(1,6,1)
-		print(str(x))
+		x = random.randrange(1,7,1)
 		roll[x-1] = roll[x-1]+1
 
 Dicerolls(n)
-print("1: " + str(roll[0]) + "/" + str(n))
-print("2: " + str(roll[1]) + "/" + str(n))
-print("3: " + str(roll[2]) + "/" + str(n))
-print("4: " + str(roll[3]) + "/" + str(n))
-print("5: " + str(roll[4]) + "/" + str(n))
-print("6: " + str(roll[5]) + "/" + str(n))
+print("1: " + str(roll[0]) + "/" + str(n) + " = " + str((roll[0]/n)*100) + "%")
+print("2: " + str(roll[1]) + "/" + str(n) + " = " + str((roll[1]/n)*100) + "%")
+print("3: " + str(roll[2]) + "/" + str(n) + " = " + str((roll[2]/n)*100) + "%")
+print("4: " + str(roll[3]) + "/" + str(n) + " = " + str((roll[3]/n)*100) + "%")
+print("5: " + str(roll[4]) + "/" + str(n) + " = " + str((roll[4]/n)*100) + "%")
+print("6: " + str(roll[5]) + "/" + str(n) + " = " + str((roll[5]/n)*100) + "%")
