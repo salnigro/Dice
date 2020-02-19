b2197cfe9b351ec5a568c083bb9c02ad402d6d05
salnigro <salnigro627@gmail.com>
2020-02-19 17:52:10 -0600

    selectionsort

---
+++ testselectionsort.py
@@ -0,0 +1,12 @@
+import sys
+A = [14,46,43,27,57,41,45,21,70]
+for i in range(len(A)): 
+
+    min_idx = i 
+    for j in range(i+1, len(A)): 
+        if A[min_idx] > A[j]: 
+            min_idx = j 
+            
+    A[i], A[min_idx] = A[min_idx], A[i] 
+
+print(A)
